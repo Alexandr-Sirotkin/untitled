75be89177eb9fbb5e570d7345f23e53ac29f30da
Alexandr Sirotkin <Aliaksandr_Sirotkin@epam.com>
2023-05-30 23:07:52 +0300

    lesson1

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,2 +1,12 @@
-package pages;public class BasePage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+  protected WebDriver driver;
+
+  public BasePage(WebDriver driver) {
+    this.driver = driver;
+  }
 }
